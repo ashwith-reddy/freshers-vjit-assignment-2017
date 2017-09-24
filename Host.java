14f1b31bfabb6c9da8e99b9d5250c2b942888ee2
ashwith-reddy <32174319+ashwith-reddy@users.noreply.github.com>
2017-09-24 21:06:11 +0530

    Add files via upload

---
+++ Host.java
@@ -0,0 +1,17 @@
+package restaurant.demo;
+
+/**
+ *The host willperform his responsibilities
+ *Further it will be updated
+ */
+
+public class Host {
+	String categories;
+	String item_name;
+	void category() {
+	
+	}
+	void items_list() {
+	}
+
+}
