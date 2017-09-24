15b0be09493a547f066ce0268e202a0ffd37777c
ashwith-reddy <32174319+ashwith-reddy@users.noreply.github.com>
2017-09-24 21:37:31 +0530

    Add files via upload

---
+++ BusBoy.java
@@ -0,0 +1,21 @@
+package restaurant.demo;
+
+/**
+ *Bus boy willperform his responsibilities
+ *Further it will be updated
+ */
+
+public class BusBoy {
+	String busboy_id;
+	String busboy_name;
+	void servewater() {
+		
+	}
+	void clean() {
+	
+	}
+	void lay() {
+		
+	}
+
+}
